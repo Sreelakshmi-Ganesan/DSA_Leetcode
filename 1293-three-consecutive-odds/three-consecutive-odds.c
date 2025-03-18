ba2703578a6b7428924d697585a6b8a76b83402b
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2025-03-18 21:36:39 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (64.77%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.c
@@ -0,0 +1,13 @@
+bool threeConsecutiveOdds(int* arr, int arrSize) {
+    
+    for(int i=0;i<arrSize-2;i++)
+    {
+        if(arr[i]%2 !=0)
+        {
+            if((arr[i+1]%2 !=0) && (arr[i+2]%2 !=0) )
+              return true;
+        }
+        else continue;
+    }
+    return false;
+}
