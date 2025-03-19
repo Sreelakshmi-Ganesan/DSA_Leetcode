cb3ab9cf8345259bc1f46ea99498a772bd49e333
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2025-03-19 16:50:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (72.68%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.c
@@ -0,0 +1,14 @@
+int sumOfUnique(int* nums, int numsSize) {
+    int count[101]={0};
+    for(int i=0;i<numsSize;i++)
+    {
+       count[nums[i]]++;
+    }
+    int sum=0;
+    for(int i=0;i<101;i++)
+    {
+        if(count[i]==1)
+         sum+=i;
+    }
+    return sum;
+}
