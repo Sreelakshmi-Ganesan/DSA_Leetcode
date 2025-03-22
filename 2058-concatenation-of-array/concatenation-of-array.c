e218d28e0e741dc51bc15bfbb2a9df866e846747
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2025-03-22 09:58:04 +0530

    Time: 3 ms (9.75%) | Memory: 14.3 MB (59.65%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.c
@@ -0,0 +1,15 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+  int *res=(int *)  malloc(2*numsSize *(sizeof(int)));
+
+  for(int i=0;i<numsSize;i++)
+  {
+    res[i]=nums[i];
+    res[i+numsSize]=nums[i];
+   
+  } 
+  *returnSize=2*numsSize;
+  return res;
+}
