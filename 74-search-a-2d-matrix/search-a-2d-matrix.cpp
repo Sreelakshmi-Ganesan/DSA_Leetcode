f4715de5ba67da9d63cea32e9cc95fcd4407501c
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2025-07-20 11:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (42.02%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -8,7 +8,7 @@ public:
             int mid=(low+high)/2;
             int row=mid/n, col=mid%n;
             if(matrix[row][col]==target) return true;
-            else if(matrix[row][col]<target) low=mid+1;
+             if(matrix[row][col]<target) low=mid+1;
             else high=mid-1;
         }
         return false;
