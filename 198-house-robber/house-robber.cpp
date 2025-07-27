2fb9cf6aa93bb5542a3653ec53853eb78ad5cef3
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2025-07-27 19:25:29 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (5.71%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    long long maxsum(int ind,vector<int> &nums, int n,vector<long long>&dp)
-    {
-        if(ind==0) return nums[ind];
-        if(ind<0) return 0;
-
-        if(dp[ind]!=-1) return dp[ind];
-
-        int pick=nums[ind] + maxsum(ind-2,nums,n,dp);
-        int nopick=0+maxsum(ind-1,nums,n,dp);
-        return dp[ind]=max(pick,nopick);
-    }
+    
     long long rob(vector<int>& nums) {
         int n=nums.size();
         vector<long long>dp(n,-1);
-        return maxsum(n-1,nums,n,dp);
+        dp[0]=nums[0];
+        int neg=0;
+        for(int i=1;i<n;i++)
+        {
+            int pick=nums[i];
+            if(i>1) pick+=dp[i-2];
+            int nopick=0+dp[i-1];
+            dp[i]=max(pick,nopick);
+        }
+        return dp[n-1];
         
     }
 };
