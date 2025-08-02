9da645a59181aa74a82fadd98f3837e7afc4e028
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2025-08-03 01:50:09 +0530

    Time: 9 ms (34.87%) | Memory: 13.4 MB (19.36%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+     unordered_map<char,int> mp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+     int n=s.size();
+     int value=0;
+     for(int i=0;i<n;i++)
+     {
+        if(i+1<n && mp[s[i]]<mp[s[i+1]])
+         value-=mp[s[i]];
+         else{
+            value+=mp[s[i]];
+         }
+     }
+     return value;
+    }
+};
