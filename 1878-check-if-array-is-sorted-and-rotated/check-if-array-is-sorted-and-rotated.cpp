d3362dd609958daf8d98864884aecff4922231f3
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2025-08-22 14:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (96.23%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -3,15 +3,16 @@ public:
     bool check(vector<int>& nums) {
         int n=nums.size();
         int ct=0;
-        for(int i=0;i<n-1;i++)
+        for(int i=0;i<n-1;i++)  // there will be atmost 1 elem which is gt its next elem in a sorted rotated array
         {
             if(nums[i]>nums[i+1])
-             ct++;
+             ct++;    
         }    
         if(nums[n-1]>nums[0])
         ct++;
 
-        return (ct<=1);
+        return (ct<=1);   // ct<= 1  is also fine
+         // if there is more than 1 elem that is gt its next elem then its not sorted and rotated
         
     
     }
