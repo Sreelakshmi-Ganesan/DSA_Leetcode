b975dd2a3ca0b08620377dadaa47e00c5ae061d6
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2025-10-08 20:01:23 +0530

    Time: 1 ms (48.13%) | Memory: 9.8 MB (94.00%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left=0,right=s.length()-1;
+     int left=0,right=s.length()-1;
+     transform(s.begin(),s.end(),s.begin(), ::tolower);
+     while(left<right)
+     {  while(left<right && !isalnum(s[left])) left++;
+         while(left <right&& !isalnum(s[right])) right--;
 
-        while(left<right)
-        {   while(left<right &&!isalnum(s[left])) left++;
-            while(left<right && !isalnum(s[right])) right--;
+         if(s[left]!=s[right]) return false;
+         left++;
+         right--;
 
-            if(tolower(s[left])!=tolower(s[right]))
-            return false;
-            left++;
-            right--;
-        }
-        return true;
+
+     }
+     return true;
     }
+
 };
