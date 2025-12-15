5eeef9b723923a3f95c652b9cc473780af1ff8fd
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2025-12-15 13:25:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (36.70%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,22 +1,25 @@
 class Solution {
-    private :
-    int f(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp)   
-    {
-        if(i==0 && j==0) return grid[0][0];
-        if(i<0 || j<0) return 1e9;
-        if(dp[i][j]!=-1) return dp[i][j]; //memoization
-        int up=f(i-1,j,grid,dp)+grid[i][j];
-        int left=f(i,j-1,grid,dp)+grid[i][j];
-        return dp[i][j]=min(up,left); //memoization
-
-    }
+    
 public:
     
     int minPathSum(vector<vector<int>>& grid) {
      int m=grid.size();
      int n=grid[0].size();
-     vector<vector<int>>dp(m,vector<int>(n,-1));
-     return f(m-1,n-1,grid,dp);
+     vector<vector<int>>dp(m,vector<int>(n,0));
+     dp[0][0]=grid[0][0]; //base case
+     for(int i=0;i<m;i++)
+     {
+        for(int j=0;j<n;j++)
+        {
+            if(i==0 && j==0) continue;
+            int up=INT_MAX; //boundary setting
+            int left=INT_MAX;
+            if(i>0) up=dp[i-1][j]+grid[i][j]; // if its in boundary 
+            if(j>0) left=dp[i][j-1]+grid[i][j];
+            dp[i][j]=min(up,left);
+        }
+     }
+     return dp[m-1][n-1];
 
     }
 };
