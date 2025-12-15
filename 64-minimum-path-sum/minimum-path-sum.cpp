6c8a41f66916b6d4bd56ea837eaad310910242aa
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2025-12-15 13:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (55.75%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
     private :
-    int f(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp)   //recursion
+    int f(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp)   
     {
         if(i==0 && j==0) return grid[0][0];
         if(i<0 || j<0) return 1e9;
-        if(dp[i][j]!=-1) return dp[i][j];
+        if(dp[i][j]!=-1) return dp[i][j]; //memoization
         int up=f(i-1,j,grid,dp)+grid[i][j];
         int left=f(i,j-1,grid,dp)+grid[i][j];
-        return dp[i][j]=min(up,left);
+        return dp[i][j]=min(up,left); //memoization
 
     }
 public:
