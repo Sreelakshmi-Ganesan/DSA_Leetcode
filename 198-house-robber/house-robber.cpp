cc31ae66a2a381922f7c88af81df6417081cae2a
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2026-06-05 22:39:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (60.83%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-     int f(vector<int>&nums, int ind,vector<int>&dp)
-     {  if(ind<0) return 0; // case of empty array , n=0 then n-1 =-1 
-        if(ind==0) return nums[0];
-        if(ind==1) return max(nums[0],nums[1]);
-        if(dp[ind]!=-1) return dp[ind];
-        
-        int notake=f(nums,ind-1,dp)+0;
-        int take=INT_MIN;
-        if(ind>=2) take=f(nums,ind-2,dp)+nums[ind];
-        return dp[ind]= max(take,notake);
-        /*
-
-        if(ind < 0) return 0;
-
-        if(ind == 0) return nums[0];
-
-        int take = nums[ind] + f(nums, ind-2);
-
-        int notake = f(nums, ind-1);
-
-        return max(take, notake);
-        */
-    
-     }
+     
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return f(nums,n-1,dp);
+        vector<int>dp(n);
+        dp[0]=nums[0];
+         //dp[1]=max(nums[0],nums[1]); can be also made so that we could run loop from i=2
+        for(int i=1;i<n;i++)
+        {
+            int take=nums[i];
+            if(i>=2) take+=dp[i-2];
+            int nontake=0+dp[i-1];
+            dp[i]=max(take,nontake);
+        }
+        return dp[n-1];
     }
 };
