3c6fe034b0988190fed8c6efbbefe09ac6386af9
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2026-06-07 22:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (78.08%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,37 +1,32 @@
 class Solution {
-public:  // tabulations
+public:  // space optimization
     
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
        int m=obstacleGrid.size();
        int n=obstacleGrid[0].size();
-       vector<vector<int>>dp(m,vector<int>(n));
-
-       dp[0][0]=1;
+       vector<int>prev(n,0);  // set imaginary prev row before 0th row as full 0
 
        for(int i=0;i<m;i++)
-       {
+       {  vector<int>curr(n);
         for(int j=0;j<n;j++)
-        {   if(obstacleGrid[i][j]==1) dp[i][j]=0;   // first check for obstacle
-            else if(i==0 && j==0) dp[i][j]=1;
+        {   if(obstacleGrid[i][j]==1) curr[j]=0;   // first check for obstacle
+            else if(i==0 && j==0) curr[j]=1;
             
             else
             {
                int up=0;
                int left=0;
-              if(i>=1) up=dp[i-1][j];
-            
-
-              
-              if(j>=1) left=dp[i][j-1];
-              
-
-              dp[i][j]=left+up;
+              if(i>=1) up=prev[j];
+              if(j>=1) left=curr[j-1];
+              curr[j]=left+up;
             }
         }
 
+        prev=curr; // please dont forget the 'prev' update to curr
+
        }
 
-     return dp[m-1][n-1];
+     return prev[n-1];
 
 
     }
