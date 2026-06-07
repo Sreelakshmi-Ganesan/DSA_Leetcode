e62018df4f2ab5a868e9ec9e90857ce8e9e05c91
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2026-06-07 20:46:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (71.53%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-       ///tabulation
-    vector<vector<int>>dp(m,vector<int>(n));
-    dp[0][0]=1;
+       ///space optimization
+    vector<int>prev(n,0);   // prev befor first row taken as a 0 -row 
+
     for(int i=0;i<m;i++)
-    {
+    {   vector<int>curr(n);  
         for(int j=0;j<n;j++)
         {
-            if(i==0 && j==0) dp[i][j]=1;
+            if(i==0 && j==0) curr[j]=1;
             else{
                 int up=0;
                 int left=0;
-                if(i>=1) up=dp[i-1][j];
-                if(j>=1) left=dp[i][j-1];
+                if(i>=1) up=prev[j];
+                if(j>=1) left=curr[j-1];
 
-                dp[i][j]=up+left;
+                curr[j]=up+left;
             }
         }
+        prev=curr;
     }
-   return dp[m-1][n-1];
+   return prev[n-1];
         
     }
 };
