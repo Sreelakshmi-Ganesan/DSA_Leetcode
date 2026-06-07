828989d5b9f27f570f1aca83650116d002a33a9e
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2026-06-07 20:31:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.71%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    
+    int f(int i,int j,vector<vector<int>>&dp)
+    {    //memoization
+        if(i==0 && j==0) return 1; //grid[0][0] 
+        if(i<0 || j<0) return 0; // out of boundary
+        if(dp[i][j]!= -1) return dp[i][j];
+        int up=f(i-1,j,dp);
+        int left=f(i,j-1,dp);
+        return dp[i][j]=up+left;
+
+    }
+
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {  if(i==0 && j==0) dp[i][j]=1;
-            else{
-                 int up=0,left=0;
-              if(i>=1)    up=dp[i-1][j];
-                if(j>=1)  left=dp[i][j-1];
-                dp[i][j]=up+left;
-            }
-            }
-        }
-        return dp[m-1][n-1];
+    vector<vector<int>>dp(m,vector<int>(n,-1));
+      return f(m-1,n-1,dp);    
     }
 };
