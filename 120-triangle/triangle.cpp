b0b5488b545c1ed18ae98d952c419174e1ed0f89
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2026-06-09 18:27:18 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (59.18%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
-    //tabulation
-    int f(int i,int j,vector<vector<int>>&triangle,int row,vector<vector<int>>&dp)
-    {
-        if(i==row-1)
-        {   /*int mini=1e9;
-            for(int k=0;k<triangle[row-1].size();k++)
-            {
-              mini=min(mini,triangle[i][k]);
-            }*/
-            return triangle[i][j];
-        }
-        
-        //if(i>triangle.size() || j>triangle[row-1].size()) return 1e9;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down =triangle[i][j]+f(i+1,j,triangle,row,dp);
-        int rightdown=triangle[i][j] +f(i+1,j+1,triangle,row,dp);
-
-        return dp[i][j]=min(down,rightdown);
-    }
+    //space optimization
+  
 
     int minimumTotal(vector<vector<int>>& triangle) {
      
        int m = triangle.size();
        int n= triangle[m-1].size(); 
-       vector<vector<int>>dp(m,vector<int>(n));    
+       vector<int>front(n,0);    
        
        for(int i=0;i<n;i++)
        {
-          dp[m-1][i]=triangle[m-1][i];
+          front[i]=triangle[m-1][i];
        }
        
        for(int i=m-2;i>=0 ;i--)
-       {
+       {  vector<int>curr(n);
         for(int j=i;j>=0;j--)
         {
-            int down =triangle[i][j]+dp[i+1][j];
-        int rightdown=triangle[i][j] +dp[i+1][j+1];
+            int down =triangle[i][j]+front[j];
+        int rightdown=triangle[i][j] +front[j+1];
 
-         dp[i][j]=min(down,rightdown);
+         curr[j]=min(down,rightdown);
         }
+        front=curr;
        }
-       return dp[0][0];
+       return front[0];
     }
 };
