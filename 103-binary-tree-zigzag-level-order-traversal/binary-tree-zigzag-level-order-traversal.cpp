f8b8f32c0e02e3c5eff90d03e1680484af03b20e
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2026-06-27 11:33:07 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (16.40%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -9,31 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //using the concept of level order traversal
+ // reverse the level array for alternate levels
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-      vector<vector<int>> res;
-      if(root==NULL) return res;
-      queue<TreeNode *>q;
-      q.push(root);
-      int flag=1;
-      while(!q.empty())
-      {
-        int size=q.size();
-        vector<int>level(size);
-        for(int i=0;i<size;i++)
+        vector<vector<int>>res;
+        if(root==NULL) return res;
+        queue<TreeNode*>q;
+        int flag=1;
+        q.push(root);
+
+        while(!q.empty())
         {
-            TreeNode* temp=q.front();
-            q.pop();
-            int ind= (flag==1)?i:size-1-i;
-            level[ind]=temp->val;
-            if(temp->left) q.push(temp->left);
-            if(temp->right) q.push(temp->right);
+            int lsize=q.size();
+            vector<int>level;
+
+            for(int i=0;i<lsize;i++)
+            {
+                TreeNode*temp=q.front();
+                q.pop();
+                if(temp->left!=NULL) q.push(temp->left);
+                if(temp->right!=NULL) q.push(temp->right);
+                level.push_back(temp->val);
+            }
+           if(flag) res.push_back(level);
+           else {
+            reverse(level.begin(),level.end()); // reversing for alternate level
+            res.push_back(level);
+           }
+           flag=!flag;
         }
-        flag=1-flag;
-        res.push_back(level);
-      }
-      return res;
+        return res;
 
     }
 };
