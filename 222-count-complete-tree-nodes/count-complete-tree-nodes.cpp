b06aeffdb31535c5b705d832159ed1c56778a4e8
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2026-06-30 01:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (44.72%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ /// uses the concept of complete tree( 2^h - 1)
+int leftht(TreeNode *root)
+{   int ht=0;
+    while(root)
+    {   ht++;
+        root=root->left;
+    }
+    return ht;
+}
+
+int rightht(TreeNode* root)
+{
+    int ht=0;
+    while(root)
+    {   ht++;
+        root=root->right;
+    }
+    return ht;
+}
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        
+        if(root==NULL) return 0;
+
+        int lh=leftht(root);
+        int rh=rightht(root);
+
+        if(lh==rh) return (1<<lh)-1; // it will be complete tree , the 2^h -1 nodes
+        
+        return 1+countNodes(root->left)+countNodes(root->right); // root + left subtree + right subtree -> recursively searching to fond complete subtree
+        
+    }
+};
