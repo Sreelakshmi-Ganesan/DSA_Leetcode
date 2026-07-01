14dbb9db95fd9e6a7400f6de730b984c07bd1337
Sreelakshmi-Ganesan <sreelakshmikg15@gmail.com>
2026-07-01 19:40:57 +0530

    Time: 0 ms (100.00%) | Memory: 35.4 MB (63.88%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -10,16 +10,17 @@
  * };
  */
 class Solution {
- /// using recursion 
- /// use if else path , or if you are only using if then ent with a return nullptr
+ // using while
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
         
-        if(root==NULL) return NULL;
-        if(root->val == val) return root;
-
-        else if(val<root->val) return searchBST(root->left,val);
-        else return searchBST(root->right,val);
+        while(root)
+        {
+            if(root->val ==val) return root;
+            else if(root->val <val) root=root->right;
+            else root=root->left;
+        }
+        return NULL;
 
     }
 };
